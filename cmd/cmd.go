f91ddfc74625a4ecd96c128bc32623b9dd6d37d1
Jonathon Chew <>
2025-06-18 23:00:43 +0100

    feat: Added help command for CMD, added support for single em dash flags

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	aphrodite "github.com/jonathon-chew/Aphrodite"
 )
 
 func ParseArguments(Arguments []string) ([]string, []string) {
@@ -19,14 +21,20 @@ func ParseArguments(Arguments []string) ([]string, []string) {
 			fileName = append(fileName, argument)
 		}
 
-		if argument == "--allow" || argument == "--force" {
+		if argument == "--help" || argument == "-help" {
+			aphrodite.Colour("Colour", "Green", "Pass in at least one file\n")
+			aphrodite.Colour("Colour", "Green", "You can use thte flag allow of force to force it for unknown / unsupported file types\n")
+			aphrodite.Colour("Colour", "Green", "You can use the file type flag to choose the type of colour coding - eg comment / variable declaration \n")
+		}
+
+		if argument == "--allow" || argument == "--force" || argument == "-allow" || argument == "-force" {
 			if !strings.Contains(argument, "--") {
 				fmt.Println(argument)
 			}
 			returnArray = append(returnArray, "Allow")
 		}
 
-		if argument == "--file-type" || argument == "--fileType" || argument == "-file" {
+		if argument == "--file-type" || argument == "--filetype" || argument == "-file" {
 			if index+1 < len(Arguments) {
 				returnArray = append(returnArray, Arguments[index+1])
 			}
